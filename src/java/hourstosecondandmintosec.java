dd960154744740c65b41cbd63a4744f019c28002
ani-123 <anisha.gurumukhi@gmail.com>
2023-08-17 15:51:12 +0530

    java programs

---
+++ src/java/hourstosecondandmintosec.java
@@ -0,0 +1,12 @@
+public class hourstosecondandmintosec {
+    public static void main(String[] args) {
+        int hour =4;
+        int minutes =30;
+        int second =minutes*60;
+        int hourseconds =hour*60*60;
+
+
+        System.out.println("hour to second:" + hourseconds);
+        System.out.println("minutes to seconds:"+ second);
+    }
+}
